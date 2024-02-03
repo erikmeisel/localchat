26e512e69c0d065341ea72ada0b023f09260457f
Erik Meisel <erikmeisel@gmail.com>
2024-02-03 02:39:56 -0300

    first

---
+++ MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace chatapp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chatTextBox.Location = new System.Drawing.Point(13, 44);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.ReadOnly = true;
+            this.chatTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatTextBox.Size = new System.Drawing.Size(413, 318);
+            this.chatTextBox.TabIndex = 1;
+            this.chatTextBox.TabStop = false;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageTextBox.Location = new System.Drawing.Point(13, 368);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(332, 70);
+            this.messageTextBox.TabIndex = 0;
+            this.messageTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.messageTextBox_KeyPress);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendButton.Location = new System.Drawing.Point(351, 368);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 70);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Enviar";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(63, 13);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(363, 29);
+            this.name.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 450);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.chatTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Local Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Leave += new System.EventHandler(this.MainForm_Leave);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Media;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace chatapp
+{
+    public partial class MainForm : Form
+    {
+        private UdpClient udpClient;
+        private Thread receivingThread;
+        NotifyIcon notifyIcon;
+        public MainForm()
+        {
+            InitializeComponent();
+            name.Text = System.Environment.MachineName;
+            createNotificationIcon();
+            InitializeChat();
+            AddToStartup();
+        }
+
+        void createNotificationIcon()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Icon;// Ruta al archivo del icono
+            notifyIcon.Text = "Local Chat"; // Texto que se muestra al pasar el cursor sobre el icono
+            // Mostrar el icono en la bandeja del sistema
+            notifyIcon.Visible = true;
+            notifyIcon.MouseClick += notifyIcon_MouseClick;
+            // Agregar un menú contextual al icono
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add("Ver", (sender, e) => {
+                if (IsDisposed) Show();
+            }); // Opción para salir de la aplicación
+            contextMenu.MenuItems.Add("Salir", (sender, e) => {
+                udpClient.Close();
+                notifyIcon.Dispose();
+                Application.Exit();
+            }); // Opción para salir de la aplicación
+            notifyIcon.ContextMenu = contextMenu;
+
+        }
+        private void InitializeChat()
+        {
+            udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, 12345));
+            receivingThread = new Thread(ReceiveMessages);
+            receivingThread.Start();
+        }
+
+        private void ReceiveMessages()
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        byte[] receivedBytes = udpClient.Receive(ref remoteEndPoint);
+                        string message = Encoding.ASCII.GetString(receivedBytes);
+                        Thread.Sleep(100);
+                        // Update the UI with the received message
+                        Invoke(new Action(() => UpdateChat(message)));
+                    } catch (Exception e)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                // The UdpClient is closed, thread can exit
+                Application.Exit();
+            }
+        }
+
+        private void UpdateChat(string message)
+        {
+            string[] msgs = message.Split(':');
+            if (msgs[0]!=name.Text)
+            {
+                ShowToastNotification(message);
+            }
+            chatTextBox.AppendText($"{message}\r\n");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                Hide();
+                e.Cancel = true;
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            string message = $"{name.Text}: {messageTextBox.Text}";
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+            // Broadcast the message to the local network
+            udpClient.Send(messageBytes, messageBytes.Length, new IPEndPoint(IPAddress.Broadcast, 12345));
+            // Clear the message input
+            messageTextBox.Clear();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void MainForm_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void messageTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==13)
+            {
+                sendButton_Click(sender, new EventArgs());
+            }
+        }
+        private void ShowToastNotification(string message)
+        {
+            if (!Visible || WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.ShowBalloonTip(10000, "Mensaje", message, ToolTipIcon.Info);
+            } else
+            {
+                PlayNotificationSound();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void AddToStartup()
+        {
+            // Agregar al registro de Windows para que se inicie con el sistema
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            // El nombre que quieras darle a tu aplicación en el registro
+            string appName = "LocalChatApp";
+
+            // Ruta completa al ejecutable de tu aplicación
+            string appPath = Application.ExecutablePath;
+
+            // Asegúrate de que no esté ya agregado
+            if (key.GetValue(appName) == null)
+            {
+                key.SetValue(appName, appPath);
+            }
+        }
+        public void PlayNotificationSound()
+        {
+            bool found = false;
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"AppEvents\Schemes\Apps\.Default\Notification.Default\.Current"))
+                {
+                    if (key != null)
+                    {
+                        Object o = key.GetValue(null); // pass null to get (Default)
+                        if (o != null)
+                        {
+                            SoundPlayer theSound = new SoundPlayer((String)o);
+                            theSound.Play();
+                            found = true;
+                        }
+                    }
+                }
+            }
+            catch
+            { }
+            if (!found)
+                SystemSounds.Beep.Play(); // consolation prize
+        }
+    }
+}
